85fa5be1879abf56593b47ff2be6c3009cc04a83
KimYeeun <s2018s01@e-mirim.hs.kr>
2020-05-12 12:04:03 +0900

    중첩반복문을 이용한 별트리찍기

--- startree/Program.cs
+++ startree/Program.cs
@@ -10,6 +10,16 @@ namespace startree
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8 - i; j++)
+                    Console.Write(' ');
+                for (int j = 0; j < i + 1; j++)
+                    Console.Write('*');
+                for (int j = 1; j < i + 1; j++)
+                    Console.Write('*');
+                Console.WriteLine();
+            }
         }
     }
 }
